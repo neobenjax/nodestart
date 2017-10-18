2571fc06dfcf6a52643000efee0fec70ee149032
unknown <bsanchez@na-at.com.mx>
2017-10-18 18:51:25 -0500

    File system cargar html y desplegarlo

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var http = require('http');
-var module1 = require('./module1');
-var module2 = require('./module2');
+var fs = require('fs');
 
 function onRequest(request, response){
-  response.writeHead(200, {'Content-type':'text/plain'});
-  response.write('<h1>Pollo</h1>');
-  response.write(module1.cadena);
-  module1.moduleFunction();
-  response.write(module2.stringVar);
-  module2.funcionModule2();
-  response.end();
+  response.writeHead(200, {'Content-type':'text/html'});
+  fs.readFile('./index.html', null, function(error, data){
+    if(error){
+      response.writeHead(404);
+      response.write('No se encontró el archivo');
+    } else {
+      response.write(data);
+    }
+    response.end();
+  });
 }
 
 http.createServer(onRequest).listen(9999);
